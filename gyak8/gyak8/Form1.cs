94a0f70f788c415483931f71791308f2c978113a
Mátyás István Maróty <matyas.maroty@stud.uni-corvinus.hu>
2021-11-12 17:45:25 +0100

    labda szinezes

--- gyak8/gyak8/Form1.cs
+++ gyak8/gyak8/Form1.cs
@@ -68,7 +68,10 @@ namespace gyak8
 
         private void btnSelectBall_Click(object sender, EventArgs e)
         {
-            Factory = new BallFactory();
+            Factory = new BallFactory
+            {
+                BallColor = btnColor.BackColor
+            };
         }
 
         private void DisplayNext()
