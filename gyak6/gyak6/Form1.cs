d769005e8dfa46cadfc872052b363504d141dfb8
Mátyás István Maróty <matyas.maroty@stud.uni-corvinus.hu>
2021-11-18 11:52:36 +0100

    create diagram

--- gyak6/gyak6/Form1.cs
+++ gyak6/gyak6/Form1.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 using System.Xml;
 
 namespace gyak6
@@ -22,6 +23,8 @@ namespace gyak6
         {
             InitializeComponent();
             GetCurrencies();
+            CreateDiagram();
+
             dataGridView1.DataSource = Rates;
         }
 
@@ -56,6 +59,26 @@ namespace gyak6
                 if (unit != 0)
                     rate.Value = value / unit;
             }
+        }
+
+        public void CreateDiagram()
+        {
+            chartRateData.DataSource = Rates;
+
+            var series = chartRateData.Series[0];
+
+            series.ChartType = SeriesChartType.Line;
+            series.XValueMember = "Date";
+            series.YValueMembers = "Value";
+            series.BorderWidth = 2;
+
+            var legend = chartRateData.Legends[0];
+            legend.Enabled = false;
+
+            var chartArea = chartRateData.ChartAreas[0];
+            chartArea.AxisX.MajorGrid.Enabled = false;
+            chartArea.AxisY.MajorGrid.Enabled = false;
+            chartArea.AxisY.IsStartedFromZero = false;
         }
     }
 }
