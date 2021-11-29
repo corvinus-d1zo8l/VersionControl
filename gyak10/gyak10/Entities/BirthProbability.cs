fd1048c05a2efde79e955388ad6a709b4610dd27
Mátyás István Maróty <matyas.maroty@stud.uni-corvinus.hu>
2021-11-29 16:40:34 +0100

    create class

---
+++ gyak10/gyak10/Entities/BirthProbability.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gyak10.Entities
+{
+    public class BirthProbability
+    {
+        public int Age { get; set; }
+
+        public int NbrOfChildren { get; set; }
+
+        public double Birthprobabilities { get; set; }
+    }
+}
